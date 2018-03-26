1347da4629a70a3c22283032d6d231ed68d97629
yasuo kurihara <breitling1216@gmail.com>
2018-03-27 06:48:09 +0900

    Update: login auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'welcom/index'
+  root to: 'welcom/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/auth/:provider/callback' => 'sessions#create'
 end
