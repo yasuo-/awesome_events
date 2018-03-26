d8eaaaa4113f711025ba93f8357dc1aeece0d8e7
yasuo kurihara <breitling1216@gmail.com>
2018-03-27 06:55:22 +0900

    Update: logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,4 +4,9 @@ class SessionsController < ApplicationController
     session[:user_id] = user.user_id
     redirect_to root_path, notice: 'ログインしました。'
   end
+
+  def destroy
+    reset_session
+    redirect_to root_path, notice: 'ログアウトしました。'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root to: 'welcom/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/auth/:provider/callback' => 'sessions#create'
+  get '/auth/:provider/callback'  to: 'sessions#create'
+  get 'logout'                    to: 'sessions#destroy', as: :logout
 end
