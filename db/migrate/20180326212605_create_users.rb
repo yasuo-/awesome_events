9f6eff1c3cb082b1ffab5276a2616953228e2649
yasuo kurihara <breitling1216@gmail.com>
2018-03-27 06:30:07 +0900

    Add: user model

---
+++ db/migrate/20180326212605_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :provider,   null: false
+      t.string :string,     null: false
+      t.string :uid,        null: false
+      t.string :nickname,   null: false
+      t.string :image_url,  null: false
+
+      t.timestamps
+    end
+
+    add_index :users, [:provider, :uid], unique: true
+  end
+end
---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :user do
+    provider "MyString"
+    string "MyString"
+    uid "MyString"
+    nickname "MyString"
+    image_url "MyString"
+  end
+end
